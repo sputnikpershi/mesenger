0acffa7802bb6d93a3b24fbd5e3664a766de52e6
Kiryl <66584728+sputnikpershi@users.noreply.github.com>
2022-10-09 13:26:09 +0800

    Начало работы с патерном Координатор

---
+++ Navigation/Coordinator/Coordinator.swift
@@ -0,0 +1,10 @@
+//
+//  Coordinator.swift
+//  Navigation
+//
+//  Created by Kiryl Rakk on 9/10/22.
+//
+
+import UIKit
+
+
---
+++ Navigation/Coordinator/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  Navigation
+//
+//  Created by Kiryl Rakk on 9/10/22.
+//
+
+import Foundation
