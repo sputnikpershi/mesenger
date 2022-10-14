fd10cc3f041b96ece3fc2642d9538ff34c7c9f4b
Kiryl <66584728+sputnikpershi@users.noreply.github.com>
2022-10-14 12:53:40 +0800

    GCD
    Установка брутуса в авторизацию и работа с activityIndicator

---
+++ Navigation/Views/Profile/BruteButton.swift
@@ -0,0 +1,45 @@
+//
+//  BruteButton.swift
+//  Navigation
+//
+//  Created by Kiryl Rakk on 14/10/22.
+//
+
+import Foundation
+import UIKit
+
+class BruteButton: UIButton {
+    
+    let buttonTitle: String
+    let buttonColor: UIColor
+    var buttonAction: () -> ()
+    
+    init(title: String, color: UIColor, action: @escaping () -> ()) {
+        self.buttonColor = color
+        self.buttonTitle = title
+        self.buttonAction = action
+        super.init(frame: .zero)
+        self.setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        self.setTitle(buttonTitle, for: .normal)
+        self.backgroundColor = buttonColor
+        self.titleLabel?.font = self.titleLabel?.font.withSize(12)
+        self.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        self.layer.cornerRadius = 5
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+       
+
+    }
+    
+    @objc private func didTapButton() {
+        print("tapped")
+        buttonAction()
+    }
+}
