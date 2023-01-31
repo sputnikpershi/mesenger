25ebe4fc4fcc8029bbbd74e0395b94c678a3226f
Kiryl <66584728+sputnikpershi@users.noreply.github.com>
2023-01-31 16:00:48 +0800

    added LocalNotificationsService

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -11,8 +11,10 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private lazy var localNotificationsService = LocalNotificationsService()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        localNotificationsService.registeForLatestUpdatesIfPossible()
         return true
     }
 
